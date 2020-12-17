d5dee3319fb630112444dfff01e7e48ff3820108
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-12-16 18:03:20 -0600

    add email validation:

--- script.js
+++ script.js
@@ -18,9 +18,13 @@ function showSuccess(input) {
   formControl.className = "form-control success";
 }
 // Check email is valid
-function isValidEmail(email) {
+function checkEmail(input) {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if (re.test(input.value.trim())){
+    showSuccess(input);
+  }else{
+    showError(input, 'Email is not valid');
+  }
 }
 //Check required fields
 function checkRequired(inputArr) {
@@ -31,6 +35,16 @@ function checkRequired(inputArr) {
       showSuccess(input);
     }
   });
+}
+//Check input length
+function checkLength(input, min, max){
+  if (input.value.length < min){
+    showError(input, `${capitalizeFirstLetter(input.id)} must be at least ${min} characters`)
+  }else if (input.value.length > max){
+    showError(input, `${capitalizeFirstLetter(input.id)} must be  less than ${max} characters`)
+  }else{
+    showSuccess(input);
+  }
 }
 
 //Capitalize First Lett of Input
@@ -43,4 +57,7 @@ form.addEventListener("submit", function (e) {
   e.preventDefault();
 
   checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
 });
